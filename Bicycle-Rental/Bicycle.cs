d98cb4141742b9b1851669dbcbd295243acef70c
HgThinker <nguyenhoanggia143@gmail.com>
2023-01-10 00:19:35 +0700

    Bicycle_For_Rent

--- Bicycle-Rental/Bicycle.cs
+++ Bicycle-Rental/Bicycle.cs
@@ -16,5 +16,30 @@ namespace Bicycle_Rental
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_to_cart_button_Click(object sender, EventArgs e)
+        {
+
+        }
+        public string GetTenXe
+        {
+            set { this.TenXe.Text = value; }
+            get { return this.TenXe.Text;}
+        }
+        public string GetGia
+        {
+            set { this.GiaThue.Text = value; }
+            get { return this.GiaThue.Text; }
+        }
+        public Image GetPicture
+        {
+            set { this.pictureBox1.Image = value; }
+            get { return this.pictureBox1.Image; }
+        }
     }
 }
