4359a7fd8cdbba5f042b1e4c83785aaaa2edca21
ntdat02092002 <71538949+ntdat02092002@users.noreply.github.com>
2023-01-10 16:25:15 +0700

    Revert "Filter Bycicle"

--- Bicycle-Rental/Main.Designer.cs
+++ Bicycle-Rental/Main.Designer.cs
@@ -64,6 +64,7 @@
             this.Bike_Rental_inquiry = new Guna.UI2.WinForms.Guna2Button();
             this.General_Inquiry = new Guna.UI2.WinForms.Guna2Button();
             this.Tour_Inquiry = new Guna.UI2.WinForms.Guna2Button();
+            this.Sub_Form_panel = new System.Windows.Forms.Panel();
             this.User_panel = new Guna.UI2.WinForms.Guna2ShadowPanel();
             this.Logout_Button = new Guna.UI2.WinForms.Guna2Button();
             this.Dashboard_Button = new Guna.UI2.WinForms.Guna2Button();
@@ -73,7 +74,6 @@
             this.Admin_btn = new Guna.UI2.WinForms.Guna2Button();
             this.Dashboard_admin_btn = new Guna.UI2.WinForms.Guna2Button();
             this.Profile_Admin_btn = new Guna.UI2.WinForms.Guna2Button();
-            this.Sub_Form_panel = new System.Windows.Forms.Panel();
             this.guna2Panel1.SuspendLayout();
             this.guna2ShadowPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -944,8 +944,14 @@
             this.Tour_Inquiry.Click += new System.EventHandler(this.Tour_Inquiry_Click);
             this.Tour_Inquiry.MouseLeave += new System.EventHandler(this.Contact_panel_MouseLeave);
             // 
-
-
+            // Sub_Form_panel
+            // 
+            this.Sub_Form_panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Sub_Form_panel.Location = new System.Drawing.Point(0, 97);
+            this.Sub_Form_panel.Name = "Sub_Form_panel";
+            this.Sub_Form_panel.Size = new System.Drawing.Size(1182, 511);
+            this.Sub_Form_panel.TabIndex = 5;
+            // 
             // User_panel
             // 
             this.User_panel.BackColor = System.Drawing.Color.Transparent;
@@ -1154,24 +1160,12 @@
             this.Profile_Admin_btn.Click += new System.EventHandler(this.Profile_Admin_btn_Click);
             this.Profile_Admin_btn.MouseLeave += new System.EventHandler(this.User_panel_MouseLeave);
             // 
-            // Sub_Form_panel
-            // 
-            this.Sub_Form_panel.BackColor = System.Drawing.Color.Transparent;
-            this.Sub_Form_panel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.Sub_Form_panel.Location = new System.Drawing.Point(0, 104);
-            this.Sub_Form_panel.Name = "Sub_Form_panel";
-            this.Sub_Form_panel.Size = new System.Drawing.Size(1184, 512);
-            this.Sub_Form_panel.TabIndex = 5;
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1184, 616);
-
+            this.ClientSize = new System.Drawing.Size(1182, 608);
             this.Controls.Add(this.Admin_panel);
             this.Controls.Add(this.User_panel);
             this.Controls.Add(this.Contact_panel);
@@ -1227,6 +1221,7 @@
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2ShadowPanel Contact_panel;
         private Guna.UI2.WinForms.Guna2Button Tour_Inquiry;
+        private System.Windows.Forms.Panel Sub_Form_panel;
         private Guna.UI2.WinForms.Guna2Button Join_A_Group_Tour;
         private Guna.UI2.WinForms.Guna2Button Make_A_Payment;
         private Guna.UI2.WinForms.Guna2Button How_To_Book;
@@ -1243,9 +1238,7 @@
         private Guna.UI2.WinForms.Guna2Button Admin_btn;
         private Guna.UI2.WinForms.Guna2Button Dashboard_admin_btn;
         private Guna.UI2.WinForms.Guna2Button Profile_Admin_btn;
-
-        private System.Windows.Forms.Panel Sub_Form_panel;
-
+        public Guna.UI2.WinForms.Guna2Button Login;
     }
 }
 
